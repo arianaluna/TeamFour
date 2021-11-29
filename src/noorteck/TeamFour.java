078b41eaa178cc09ef6ec0e983a94c013834afa6
Martian-Marvin <94877581+Martian-Marvin@users.noreply.github.com>
2021-11-29 08:16:31 -0500

    Marvin changnes

--- src/noorteck/TeamFour.java
+++ src/noorteck/TeamFour.java
@@ -5,6 +5,11 @@ public class TeamFour {
 		System.out.println("Hello Team");
 		
 		System.out.println("Hello Ariana");
+		
+		System.out.println("Marvin was here");
+		// I noticed that our TeamFour repository from class has the same name as this one
+		// I had to change the name when importing to avoid a conflict
+		// I named it TeamFourAriana in-case anyone else goes through the same issue
 	}
 
 }
