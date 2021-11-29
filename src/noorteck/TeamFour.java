5abdf85e88867ab1271a0aa510da540873143d9c
Fieruz1 <94871198+Fieruz1@users.noreply.github.com>
2021-11-29 14:29:01 -0500

    Update TeamFour.java
    I return it back

--- src/noorteck/TeamFour.java
+++ src/noorteck/TeamFour.java
@@ -1,6 +1,6 @@
 package noorteck;
 
-public class TeamFourAriana {
+public class TeamFour {
 	public static void main(String[] args) {
 		System.out.println("Hello Team");
 		
@@ -10,6 +10,8 @@ public class TeamFourAriana {
 		// I noticed that our TeamFour repository from class has the same name as this one
 		// I had to change the name when importing to avoid a conflict
 		// I named it TeamFourAriana in-case anyone else goes through the same issue
+		System.out.println("Hi Fieruz was here");
+		// I did the same Marvin did since the projectname is the same
 	}
 
 }
